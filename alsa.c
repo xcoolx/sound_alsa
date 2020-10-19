78d093673bbe689818b90d9d32581e8ff7fd56ae
Dmytro.Kovalov <kovalov.d.a@gmail.com>
2020-10-19 20:25:17 +0300

    Add files via upload

--- alsa.c
+++ alsa.c
@@ -25,7 +25,7 @@
 #include <stdio.h>
 
 //#define PCM_DEVICE "mix_playback"
-#define PCM_DEVICE "default"
+static char PCM_DEVICE[64] = {'d','e','f','a','u','l','t'};
 
 enum format {
     sample_8     = 1,
@@ -52,6 +52,7 @@ struct pcm_parameters {
 	int rate;
         int channels;	
 	snd_pcm_uframes_t period;
+    snd_pcm_uframes_t buffer;
         snd_pcm_format_t pcm_format;
         unsigned buffer_time;
 	unsigned period_time;
@@ -200,30 +201,46 @@ static int hw_params_set(snd_pcm_t *pcm_handle, snd_output_t *log, struct pcm_pa
         }
 
 	pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &expected->rate, &dir);
-        if(pcm < 0) {
-		printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
-                goto error_handling;
-        }
+    if(pcm < 0) {
+        printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
+        goto error_handling;
+    }
 
-        pcm = snd_pcm_hw_params_set_period_time_near(pcm_handle, params, &expected->period_time, 0);
-        if(pcm < 0) {
-                printf("ERROR: Can't set period_time. %s\n", snd_strerror(pcm));
-                goto error_handling;
-        }
+    pcm = snd_pcm_hw_params_get_buffer_time_max(params, &expected->buffer_time, 0);
+    if(pcm < 0) {
+        printf("ERROR: Can't get buffer time max. %s\n", snd_strerror(pcm));
+        goto error_handling;
+    }
 
-        pcm  = snd_pcm_hw_params_set_buffer_time_near(pcm_handle, params, &expected->buffer_time, 0);
-        if(pcm < 0) {
-                printf("ERROR: Can't set buffer time. %s\n", snd_strerror(pcm));
-                goto error_handling;
-        }
+    if (expected->buffer_time > 500000) expected->buffer_time = 500000;
+    expected->period_time = expected->buffer_time / 4;
+    
+    pcm = snd_pcm_hw_params_set_period_time_near(pcm_handle, params, &expected->period_time, 0);
+    if(pcm < 0) {
+        printf("ERROR: Can't set period_time. %s\n", snd_strerror(pcm));
+        goto error_handling;
+    }
 
-        /* Write parameters */
-        pcm = snd_pcm_hw_params(pcm_handle, params);
-        if(pcm < 0) {
-                printf("ERROR: Can't set hardware parameters. %s\n", snd_strerror(pcm));
-                goto error_handling;
-        }
+    pcm  = snd_pcm_hw_params_set_buffer_time_near(pcm_handle, params, &expected->buffer_time, 0);
+    if(pcm < 0) {
+            printf("ERROR: Can't set buffer time. %s\n", snd_strerror(pcm));
+            goto error_handling;
+    }
 
+    /* Write parameters */
+    pcm = snd_pcm_hw_params(pcm_handle, params);
+    if(pcm < 0) {
+            printf("ERROR: Can't set hardware parameters. %s\n", snd_strerror(pcm));
+            goto error_handling;
+    }
+
+    printf( "format       :%d\n", (int)expected->pcm_format);
+	printf( "channels     :%d\n", (int)expected->channels);
+	printf( "rate         :%d\n", (int)expected->rate);
+	printf( "p time       :%d\n", (int)expected->period_time);
+	printf( "b time       :%d\n", (int)expected->buffer_time);
+	printf( "p size       :%d\n", (int)0);
+	printf( "b size       :%d\n", (int)0);
 	/* Resume information */
     
 	pcm = snd_pcm_hw_params_get_channels(params, &expected->channels);
@@ -239,6 +256,11 @@ static int hw_params_set(snd_pcm_t *pcm_handle, snd_output_t *log, struct pcm_pa
 	pcm = snd_pcm_hw_params_get_period_size(params, &expected->period, 0);
 	if (pcm < 0) {
 		printf("ERROR: can't get period size: %s\n", snd_strerror(pcm));
+		goto error_handling;
+	}
+    pcm = snd_pcm_hw_params_get_buffer_size(params, &expected->buffer);
+	if (pcm < 0) {
+		printf("ERROR: can't get buffer size: %s\n", snd_strerror(pcm));
 		goto error_handling;
 	}
 	pcm = snd_pcm_hw_params_get_format(params, &expected->pcm_format);
@@ -278,13 +300,13 @@ static int sw_params_set(snd_pcm_t *pcm_handle, snd_output_t *log, struct pcm_pa
                 goto error_handling;
         }
 
-	pcm = snd_pcm_sw_params_set_start_threshold(pcm_handle, params, expected->period * 4);
+	pcm = snd_pcm_sw_params_set_start_threshold(pcm_handle, params, expected->buffer);
         if(pcm < 0) {
                 printf("ERROR: Can't set sw start threshold. %s\n", snd_strerror(pcm));
                 goto error_handling;
         }
 
-	pcm = snd_pcm_sw_params_set_stop_threshold(pcm_handle, params, expected->period * 4);
+	pcm = snd_pcm_sw_params_set_stop_threshold(pcm_handle, params, expected->buffer);
         if(pcm < 0) {
                 printf("ERROR: Can't set sw stop threshold. %s\n", snd_strerror(pcm));
                 goto error_handling;
@@ -304,7 +326,7 @@ static int sw_params_set(snd_pcm_t *pcm_handle, snd_output_t *log, struct pcm_pa
 int main(int argc, char **argv) {
         snd_output_t *log = NULL;
         snd_pcm_t *pcm_handle = NULL;
-        snd_pcm_uframes_t write = 0;
+        snd_pcm_uframes_t write_frames = 0;
         unsigned int shift = 0;
 	char *buff = NULL;
         int pcm_format;
@@ -314,11 +336,10 @@ int main(int argc, char **argv) {
         struct pcm_parameters actual = {0};
         struct pcm_parameters expected = {0};
 
+	printf("Usage: %s <pcm device name> <sample_rate> <format> <channels>\n", argv[0]);
 
-	printf("Usage: %s <sample_rate> <format> <channels> <buffer_time>\n", argv[0]);
-
-        printf("\n************************\n");
-        printf("available <format> list:\n");
+    printf("\n************************\n");
+    printf("available <format> list:\n");
 	printf("Sample_8     = 1\n");
 	printf("Sample_16    = 2\n");
 	printf("Sample_24_3  = 3\n");
@@ -332,8 +353,11 @@ int main(int argc, char **argv) {
         	goto error_handling;
 	}
 
-	expected.rate = atoi(argv[1]);
-        pcm_format = atoi(argv[2]);
+    memset(PCM_DEVICE, 0, sizeof(PCM_DEVICE));
+    memcpy(PCM_DEVICE, argv[1], strlen(argv[1]));
+
+	expected.rate = atoi(argv[2]);
+    pcm_format = atoi(argv[3]);
         switch(pcm_format) {
            case sample_8:
                expected.pcm_format = SND_PCM_FORMAT_S8;
@@ -359,9 +383,7 @@ int main(int argc, char **argv) {
            default:
                goto error_handling;
         }
-        expected.channels = atoi(argv[3]);
-        expected.buffer_time = atoi(argv[4]);
-	expected.period_time = expected.buffer_time / 4;
+        expected.channels = atoi(argv[4]);
         
         if(SND_PCM_FORMAT_LAST == pcm_format) {
             goto error_handling;
@@ -369,6 +391,10 @@ int main(int argc, char **argv) {
 
         memcpy(&actual, &expected, sizeof(struct pcm_parameters));
         pcm = snd_output_stdio_attach(&log, stdout, 0);
+        if (pcm < 0) {
+                printf("ERROR: Can't attach log %s\n", snd_strerror(pcm));
+                goto error_handling;
+        }
 	/* Open the PCM device in playback mode */
 	pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE,
 					SND_PCM_STREAM_PLAYBACK, 0);
@@ -378,10 +404,6 @@ int main(int argc, char **argv) {
                 goto error_handling;
         }
 
-        if (pcm < 0) {
-                printf("ERROR: Can't attach log %s\n", snd_strerror(pcm));
-                goto error_handling;
-        }
         pcm = hw_params_set(pcm_handle, log, &actual);
         if (pcm < 0) {
                 printf("HW parameters set fail\n");
@@ -414,21 +436,28 @@ int main(int argc, char **argv) {
         snd_pcm_dump(pcm_handle, log);
         printf("*******************************************\n");
 
+        int fd = 0;
+        fd = open("test.txt",  O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+
+        tmp = read(STDIN_FILENO, buff, 2);
         do {
             tmp = read(STDIN_FILENO, buff, actual.period_bytes);
+            write(fd,buff,actual.period_bytes);
+            
             if(tmp != actual.period_bytes) {
                  printf("read bytes %d expected %d\n", tmp, actual.period_bytes);
             }
             if (tmp) {
                 do {
-                    pcm = snd_pcm_writei(pcm_handle, buff + shift, actual.period - write);
+                    pcm = snd_pcm_writei(pcm_handle, buff + shift, actual.period - write_frames);
+
                     if(pcm > 0) {
-                        write += pcm;
-                        shift = write * actual.sample_size;
+                        write_frames += pcm;
+                        shift = write_frames * actual.sample_size;
                     }
-                } while(pcm == -EAGAIN || (pcm >= 0 && write < actual.period));
+                } while(pcm == -EAGAIN || (pcm >= 0 && write_frames < actual.period));
                 shift = 0;
-                write = 0;
+                write_frames = 0;
 
                 if(pcm < 0) {
                     if (pcm == -ESTRPIPE) {
@@ -453,6 +482,7 @@ error_handling:
         }
         if(log) snd_output_close(log);
 	if(buff) free(buff);
+        if(fd) close(fd);
 
 	return pcm;
 }
